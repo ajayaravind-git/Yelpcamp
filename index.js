2a1e0a7d20271aed8c1636ff0aca09d30c371db5
Ajay Aravind <ajayaravind.me@gmail.com>
2022-11-17 17:30:42 +0530

    add start script

--- index.js
+++ index.js
@@ -175,8 +175,8 @@ app.use((err, req, res, next) => {
 
 })
 
-
-app.listen(3000, () => {
+const port = process.env.PORT||3000
+app.listen(port, () => {
     console.log('serving on port 3000');
 })
 //
